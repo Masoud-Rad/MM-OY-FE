37e7d63aeb893808ba485be78f5a2a9309f2c256
Masoud <masoud.rad.uk@gmail.com>
2025-01-05 22:19:26 +0000

    Add theme toggle button to Header component

--- mm-oy-fe-react/src/App.jsx
+++ mm-oy-fe-react/src/App.jsx
@@ -1,22 +1,24 @@
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import MainPage from "./components/MainPage"
-import Review from "./components/Review"
-import SecondPage from "./components/SecondPage"
-
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainPage from "./components/MainPage";
+import Review from "./components/Review";
+import SecondPage from "./components/SecondPage";
 
 function App() {
+  const [theme, setTheme] = React.useState("light");
 
   return (
     <>
-    <Header />
-    <MainPage />
-    <SecondPage />
-    <Review />
-    <Footer />
-
+      <div data-bs-theme={theme === "light" ? "light" : "dark"}>
+        <Header setTheme={setTheme} them={theme}/>
+        <MainPage />
+        <SecondPage />
+        <Review />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- mm-oy-fe-react/src/components/Header.jsx
+++ mm-oy-fe-react/src/components/Header.jsx
@@ -1,65 +1,79 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { IoMenu } from "react-icons/io5";
-import '../styles.css';
+import { MdOutlineLightMode } from "react-icons/md";
+import { MdOutlineDarkMode } from "react-icons/md";
+import "../styles.css";
 
-export const Header = () => {
-
-
-  
+export const Header = ({ setTheme, theme }) => {
   const [companyName, setCompanyName] = React.useState("");
-  const [logoUrl,setlogoUrl] = React.useState("");
+  const [logoUrl, setlogoUrl] = React.useState("");
   const [isCollapsed, setIsCollapsed] = React.useState(true);
 
-
-  const fetchCompanyData = async () => {  
-      try {
-          const response = await fetch('https://mm-oy.onrender.com/api/companyDetails');
-          const data = await response.json(); 
-          setCompanyName(data.companyDetails[0].company_name);
-          setlogoUrl(data.companyDetails[0].logo_url);
-      } catch (error) {
-          console.error('Error fetching company info:',error);
-      }
+  const fetchCompanyData = async () => {
+    try {
+      const response = await fetch(
+        "https://mm-oy.onrender.com/api/companyDetails"
+      );
+      const data = await response.json();
+      setCompanyName(data.companyDetails[0].company_name);
+      setlogoUrl(data.companyDetails[0].logo_url);
+    } catch (error) {
+      console.error("Error fetching company info:", error);
     }
-      useEffect(() => {
-        fetchCompanyData();
-      }, []);
+  };
+  useEffect(() => {
+    fetchCompanyData();
+  }, []);
 
-      const toggleCollapse = () => {
-        setIsCollapsed(!isCollapsed);
-      };
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
 
-  return (<>
+  const handleThemeChange = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
 
-  <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    
-    <button type="button" className="btn" onClick={toggleCollapse}>
-    <IoMenu size={30}/>
-    </button>
-    <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/About">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/Contact">Contact</a>
-        </li>
-        
-      </ul>
-    </div>
-    <div className="d-flex">
-        <img src={logoUrl} alt="company's logo" className='company-logo'/>
-    </div>
-  </div>
-</nav>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button type="button" className="btn" onClick={toggleCollapse}>
+            <IoMenu size={30} />
+          </button>
+          <div
+            className={`collapse navbar-collapse ${isCollapsed ? "" : "show"}`}
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/About">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/Contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div >
+          <button type="button" className="btn" onClick={handleThemeChange}>
+              {theme === 'light' ? <MdOutlineDarkMode size={24} /> : <MdOutlineLightMode size={24} />}
+            </button>
+          </div>
+          <div className="d-flex">
+            <img src={logoUrl} alt="company's logo" className="company-logo" />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-
- 
-  </>)
-}
-
-export default Header
+export default Header;
